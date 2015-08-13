771ca8d211871b4f04df0badf5d27b108c514715
Alexandra Leisse <troubalex@gmail.com>
2015-08-13 13:46:21 +0200

    Lean UX at Iterate conference

---
+++ presentations/lean_ux_iterate_konferanse_2015.rb
@@ -0,0 +1,199 @@
+center <<-EOS
+  \e[1mLean UX in a nutshell\e[0m
+
+
+  Alexandra Leisse
+  @troubalex
+
+  Iterate konferanse 2015
+EOS
+
+center <<-EOS
+	\"I just love reading requirements documentation!\"
+
+	-- Someone, somewhere
+EOS
+
+center <<-EOS
+	\"I just love \e[1mwriting\e[0m requirements documentation!\"
+
+	-- Nobody, ever
+EOS
+
+center <<-EOS
+	\"Get out of the deliverables business.\"
+
+	-- Jeff Gotthelf
+EOS
+
+section "The three Foundations of Lean UX" do
+	center <<-EOS
+		Design Thinking
+	EOS
+	center <<-EOS
+		Agile Software Development
+	EOS
+	center <<-EOS
+		Lean Startup Method
+	EOS
+end
+
+section "Design Thinking" do
+	block <<-EOS
+			“Design thinking is a human-centered approach to innovation 
+
+			that draws from the designer's toolkit 
+
+			to integrate the needs of people, 
+
+			the possibilities of technology, 
+
+			and the requirements for business success.” 
+
+						-- Tim Brown, president and CEO of IDEO
+	EOS
+	block <<-EOS
+		The three Spaces of Design Thinking
+
+		+ Inspiration
+
+		+ Ideation
+
+		+ Implementation
+	EOS
+		block <<-EOS
+		The three Constraints of Design Thinking
+
+		+ Viability (Business)
+
+		+ Desirability (Human)
+
+		+ Feasibility (Technical)
+	EOS
+end
+
+section "Lean UX" do
+	block <<-EOS
+			"Lean UX is the practice 
+
+			of bringing the true nature of a product to light faster, 
+
+			in a collaborative, cross-functional way 
+			
+			that reduces the emphasis on thorough documentation 
+			
+			while increasing the focus on building a shared understanding 
+			
+			of the actual product experience being designed."
+	EOS
+end
+
+section "Principles" do
+  center <<-EOS
+		Principle #1
+
+		Cross-functional Teams
+	EOS
+	center <<-EOS
+		Principle #2
+  	
+		Small, Dedicated, Colocated
+	EOS
+	center <<-EOS
+		Principle #3
+  	
+		Progress = Outcomes, Not Output
+	EOS
+	center <<-EOS
+		Principle #4
+  	
+		Problem-Focused Teams
+	EOS
+	center <<-EOS
+		Principle #5
+
+		Removing Waste
+	EOS
+	center <<-EOS
+		Principle #6
+
+		Small Batch Size
+	EOS
+	center <<-EOS
+		Principle #7
+
+		Continuous Discovery
+	EOS
+	center <<-EOS
+		Principle #8
+
+		GOOB
+	EOS
+	center <<-EOS
+		Principle #9
+
+		Shared Understanding
+	EOS
+	center <<-EOS
+		Principle #10
+
+		Anti-Pattern: Rockstars, Gurus, and Ninjas
+	EOS
+	center <<-EOS
+		Principle #11
+
+		Externalising Your Work
+	EOS
+	center <<-EOS
+		Principle #12
+
+		Making over Analysis
+	EOS
+	center <<-EOS
+		Principle #13
+
+		Learning over Growth
+	EOS
+	center <<-EOS
+		Principle #14
+
+		Permission to Fail
+	EOS
+	center <<-EOS
+		Principle #15
+
+		Getting out of the Deliverables Business
+	EOS
+end
+
+section "A Practical Example" do
+end
+
+section "Take Aways" do
+	block <<-EOS
+				"Instead, designing together 
+
+				increases the design IQ of the entire team. 
+
+				[...] 
+
+				This collaboration, in turn, 
+
+				breeds increased feeling of ownership
+
+				over the work being done [...]."
+	EOS
+
+	block <<-EOS
+				"Finally, collaborative design builds 
+
+				team-wide shared understanding. 
+
+				It is shared understanding that is the 
+
+				currency of Lean UX." 
+	EOS
+end
+
+section "Takk for meg" do
+end
